d81c148d71b2e5b4c89e7b7522e0e952355068e4
sonntagsgesicht <sonntagsgesicht@icloud.com>
2019-07-26 13:57:06 +0200

    adding missed into to each new file

--- businessdate/businessperiod.py
+++ businessdate/businessperiod.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#  businessdate
+#  ------------
+#  A fast, efficient Python library for generating business dates inherited
+#  from float for fast date operations. Typical banking business methods
+#  are provided like business holidays adjustment, day count fractions.
+#  Beside dates generic business periods offer to create time periods like
+#  '10Y', '3 Months' or '2b'. Periods can easily added to business dates.
+#
+#  Author:  pbrisk <pbrisk_at_github@icloud.com>
+#  Copyright: 2016, 2017 Deutsche Postbank AG
+#  Website: https://github.com/pbrisk/businessdate
+#  License: APACHE Version 2 License (see LICENSE file)
+
 
 from datetime import timedelta
 
--- businessdate/businessschedule.py
+++ businessdate/businessschedule.py
@@ -1,4 +1,20 @@
-from businessdate import BusinessDate, BusinessPeriod
+# -*- coding: utf-8 -*-
+
+#  businessdate
+#  ------------
+#  A fast, efficient Python library for generating business dates inherited
+#  from float for fast date operations. Typical banking business methods
+#  are provided like business holidays adjustment, day count fractions.
+#  Beside dates generic business periods offer to create time periods like
+#  '10Y', '3 Months' or '2b'. Periods can easily added to business dates.
+#
+#  Author:  pbrisk <pbrisk_at_github@icloud.com>
+#  Copyright: 2016, 2017 Deutsche Postbank AG
+#  Website: https://github.com/pbrisk/businessdate
+#  License: APACHE Version 2 License (see LICENSE file)
+
+from businessperiod import BusinessPeriod
+from businessdate import BusinessDate
 from businessrange import BusinessRange
 
 
